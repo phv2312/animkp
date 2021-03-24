437ccc9ef63aca6eb5e00e03d99b90d2fcf6ff92
phv2312 <kat87yb@gmail.com>
2021-03-24 22:15:20 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='animkp',
       description='Anime Keypoint Estimation .',
       version='0.1.0',
 
-      packages=['pose_estimator'],
+      packages=['pose_estimator', 'pose_estimator.wrapper', 'pose_estimator.wrapper.pose_anime'],
       install_requires=requirements,
       include_package_data=True,
       zip_safe=False)
